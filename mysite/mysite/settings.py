8ac22708a93cb57c830824b67a70eebf1cfd971b
Neal <33704976+nealmick@users.noreply.github.com>
2023-05-11 15:47:26 -0400

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -155,7 +155,6 @@ DEFAULT_RENDERER_CLASSES = [
 
 DEFAULT_AUTHENTICATION_CLASSES = [
     'rest_framework.authentication.SessionAuthentication',
-    'rest_framework.authentication.SessionAuthentication'
 
 ]
 if DEBUG:
